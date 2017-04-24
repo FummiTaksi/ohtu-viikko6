96160c05367b1e01285df8d47c470516f33a0553
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-04-24 13:49:09 +0300

    T3 tehty

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -5,12 +5,12 @@ import statistics.matcher.*;
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
+
+        Matcher m = new Not( new HasFewerThan(10, "goals"),
+                             new HasFewerThan(10, "assists"),
                              new PlaysIn("PHI")
         );
-        
+
         for (Player player : stats.matches(m)) {
             System.out.println( player );
         }
